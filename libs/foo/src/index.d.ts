22f439414097a77b1fe97a6b8b058a58b87031c3
dhythm <yuta.okada.20@gmail.com>
2025-01-13 12:55:10 +0900

    feat: rename index.ts _index.ts and change output dir to libs/foo

---
+++ libs/foo/src/index.d.ts
@@ -0,0 +1,7 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+
+interface FooProps {
+}
+declare function Foo(props: FooProps): react_jsx_runtime.JSX.Element;
+
+export { Foo, type FooProps };
